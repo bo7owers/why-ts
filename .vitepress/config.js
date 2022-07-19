963c2c1b59bb47c5e707872736adcc4c9838963e
bo7owers <rene7orres@gmail.com>
2022-07-18 22:46:46 -0400

    Add resources, tweak config file

--- .vitepress/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-  title: "Why TS",
-  description: "Quick explanation of the benefits of Typescript",
-};
--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,21 @@
+const guideSidebar = {
+  text: "Guide",
+  items: [{ text: "Types of types", link: "/guide/types-of-types" }],
+};
+
 export default {
-  title: "Why TS",
+  title: "Why TS?",
   description: "Quick explanation of the benefits of Typescript",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Quick Guide", link: "/guide/" },
+      { text: "Resources", link: "/resources/" },
+    ],
+    sidebar: { "/guide/": [guideSidebar] },
+    editLink: {
+      pattern: "https://github.com/bo7owers/why-ts/edit/main/docs/:path",
+      text: "Edit this page on GitHub",
+    },
+  },
 };
